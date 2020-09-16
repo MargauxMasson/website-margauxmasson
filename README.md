c08d248b43d3108ff059143e8a3392e8de3acc53
Margaux <mmasson@lucidyne.com>
2020-09-16 13:13:19 -0700

    Added latest projects (WIP) and new features

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 ## Usage
 
-After installation, run `npm install` and then run `gulp dev` which will open up a preview of the template in your default browser, watch for changes to core template files, and live reload the browser when changes are saved. You can view the `gulpfile.js` to see which tasks are included with the dev environment.
+After installation, run `npm install` and then run `gulp dev` which will open up a preview of the template
+ in your default browser, watch for changes to core template files, 
+ and live reload the browser when changes are saved. 
+ You can view the `gulpfile.js` to see which tasks are included with the dev environment.
+If credentials issues with firebase: `firebase logout` and then `firebase login`
 
 #### Gulp Tasks
 
--- js/creative.js
+++ js/creative.js
@@ -15,6 +15,43 @@
     }
   });
 
+
+  $('.featured-post-slider').slick({
+    dots: false,
+    speed: 300,
+    autoplay: true,
+    arrows: false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2
+        }
+      }
+    ]
+  });
+
+  // Masonry
+  $(document).ready(function () {
+    $('.masonry-container').masonry({
+      itemSelector: '.masonry-container > div',
+      columnWidth: 1
+    });
+  });
+
   // Closes responsive menu when a scroll trigger link is clicked
   $('.js-scroll-trigger').click(function() {
     $('.navbar-collapse').collapse('hide');
